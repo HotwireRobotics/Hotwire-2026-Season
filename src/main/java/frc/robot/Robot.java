06ad2090e18751e3cad627a58cf167dcba4dc687
Hotwire Drive Station <hotwire2990@gmail.com>
2026-02-28 16:25:49 -0800

    navgrid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -73,7 +73,9 @@ public class Robot extends LoggedRobot {
 
     robotContainer = new RobotContainer();
 
-    Double[] robotpose = {robotContainer.drive.getPose().getX(), robotContainer.drive.getPose().getX()};
+    Double[] robotpose = {
+      robotContainer.drive.getPose().getX(), robotContainer.drive.getPose().getX()
+    };
     SmartDashboard.putNumberArray("robot-pose", robotpose);
 
     SmartDashboard.putNumber("Shooter RPM", robotContainer.shooterPower);
